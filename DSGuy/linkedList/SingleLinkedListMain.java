9ff2fb28ba71f81a93d1b2988f339a2d70d9fc87
Puru Chandra <puruchandra4@gmail.com>
2019-09-15 16:49:20 +0530

    linked list deletion and searching

--- DSGuy/linkedList/SingleLinkedListMain.java
+++ DSGuy/linkedList/SingleLinkedListMain.java
@@ -14,5 +14,30 @@ class SingleLinkedListMain {
         slo.insert(20, 2);
         slo.traverseList();
 
+        slo.insert(50, 0);
+        slo.insert(40, 0);
+        slo.traverseList();
+
+        slo.searchLinkedList(40);
+
+        slo.deleteFromLinkedList(0);
+        slo.traverseList();
+
+        slo.deleteFromLinkedList(0);
+        slo.traverseList();
+
+        slo.insert(40, 3);
+        slo.insert(80, 0);
+        slo.insert(70, 0);
+        slo.insert(60, 0);
+        slo.traverseList();
+
+        slo.deleteFromLinkedList(2);
+        System.out.println("after deletion");
+        slo.traverseList();
+
+        slo.deleteEntireList();
+        System.out.println("traversing after deletion of entire list");
+        slo.traverseList();
     }
 }
